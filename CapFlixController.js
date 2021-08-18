30b40445f056031c0521422b30d7da6e894cc311
Ardura Gonzalez <uo257493@uniovi.es>
2021-08-18 10:37:38 +0200

    Limpieza, no imagen, autobusqueda

--- CapFlixController.js
+++ CapFlixController.js
@@ -1,10 +1,7 @@
-
-
-const API_KEY = "7bdea3a6";
+const API_KEY = "1e1e7271";
 const URL_ALL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`;
 const URL_DETAIL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=`;
 var lastQuery =""
-
 const result = document.getElementById('result');
 
 function searchMovies() {
@@ -14,7 +11,7 @@ function searchMovies() {
 }
 
 async function fetchMovies(query) {
-    const respuesta = await fetch(URL_ALL+query, {
+    const respuesta = await fetch(URL_ALL+query+"&page=1", {
         method: 'GET', 
     mode: 'cors', 
     credentials: 'same-origin', 
@@ -41,19 +38,8 @@ async function fetchmovie(query){
 function getMovies(query) {
     var result = document.getElementById("result");
     result.innerHTML = "";
-    let movies= [];
-    fetchMovies(query).then(response => movies = response.Search.forEach(movie => {
-        let div = document.createElement("div");
-        div.className = "col-1 movie"
-        let img = document.createElement("img");
-        img.src = movie.Poster;
-        img.className = "img-fluid"
-        img.onclick = function () {
-            getDetail(movie);
-        };
-        div.appendChild(img);
-        document.getElementById("result").appendChild(div);
-    }))
+    
+    fetchMovies(query).then(response => listaPelis(response))
 
 
 
@@ -62,52 +48,103 @@ function getMovies(query) {
 
 function getMovie(query) {
     console.log("desde getmovie");
-    fetchmovie(query).then(response => {
-        document.getElementById("result").innerHTML = "";
-        let img = document.createElement("img");
-        img.className = "img-fluid";
-        img.src = response.Poster;
-        let div = document.createElement("div");
-        div.className="card"
-        let title = document.createElement("div");
-        title.className = "card-title";
-        let h1 = document.createElement("h1")
-        h1.innerHTML = response.Title;
-        title.appendChild(h1);
-        div.appendChild(title);
-        let cardBody = document.createElement("div");
-        cardBody.className = "card-body";
-        let year = document.createElement("p");
-        year.innerHTML = response.Year
-        cardBody.appendChild(year);
-        let genre = document.createElement("p")
-        genre.innerHTML = response.Genre
-        cardBody.appendChild(genre)
-        let director = document.createElement("p");
-        director.innerHTML = "Directed by "+response.Director;
-        cardBody.appendChild(director);
-        let actors = document.createElement("p");
-        actors.innerHTML = "Starring: "+response.Actors;
-        cardBody.appendChild(actors);
-        let plot = document.createElement("p");
-        plot.innerHTML = "'"+response.Plot+"'";
-        cardBody.appendChild(plot)
-        let back = document.createElement("button");
-        back.className ="btn btn-outline-secondary";
-        back.innerHTML ="Back";
-        back.onclick = function () {
-            getMovies(lastQuery);
-        }
-        cardBody.appendChild(back)
-        div.appendChild(cardBody)
-
-        document.getElementById("result").appendChild(img);
-        document.getElementById("result").appendChild(div);
-        console.log(response)});
+    fetchmovie(query).then(response => createElementMovie(response));
 }
 
 
 function getDetail(movie) {
     getMovie(movie.Title);
 
+}
+
+function createElementMovie(response){
+    document.getElementById("result").innerHTML = "";
+    let img = document.createElement("img");
+    img.className = "img-fluid";
+    if(response.Poster=="N/A")
+        img.src = "noImage.png"
+    else
+        img.src = response.Poster;
+    let div = document.createElement("div");
+    div.className="card"
+    let title = document.createElement("div");
+    title.className = "card-title";
+    let h1 = document.createElement("h1")
+    h1.innerHTML = response.Title;
+    title.appendChild(h1);
+    div.appendChild(title);
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    let year = document.createElement("p");
+    year.innerHTML = response.Year
+    cardBody.appendChild(year);
+    let genre = document.createElement("p")
+    genre.innerHTML = response.Genre
+    cardBody.appendChild(genre)
+    let director = document.createElement("p");
+    director.innerHTML = "Directed by "+response.Director;
+    cardBody.appendChild(director);
+    let actors = document.createElement("p");
+    actors.innerHTML = "Starring: "+response.Actors;
+    cardBody.appendChild(actors);
+    let plot = document.createElement("p");
+    plot.innerHTML = "'"+response.Plot+"'";
+    cardBody.appendChild(plot)
+    let back = document.createElement("button");
+    back.className ="btn btn-outline-secondary";
+    back.innerHTML ="Back";
+    back.onclick = function () {
+        getMovies(lastQuery);
+    }
+    cardBody.appendChild(back)
+    div.appendChild(cardBody)
+
+    document.getElementById("result").appendChild(img);
+    document.getElementById("result").appendChild(div);
+    console.log(response)
+}
+
+$(document).ready(function () {
+
+
+
+  
+        let timer;
+        const input = document.getElementById("titulo");
+        input.addEventListener("keyup", (e) => {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            searchMovies();
+          }, 1300);
+        });
+
+        
+
+
+
+
+});
+
+function listaPelis(response){
+    let movies= [];
+    if(response.Response == "False"){
+        alert("Se ha producido un error")
+        return;
+    }
+
+        movies = response.Search.forEach(movie => {
+        let div = document.createElement("div");
+        div.className = "col-1 movie"
+        let img = document.createElement("img");
+        if(movie.Poster=="N/A")
+            img.src = "noImage.png"
+        else
+            img.src = movie.Poster;
+        img.className = "img-fluid"
+        img.onclick = function () {
+            getDetail(movie);
+        };
+        div.appendChild(img);
+        document.getElementById("result").appendChild(div);
+    })
 }
